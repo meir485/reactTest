6b306066910263837e18c39ffeb086614dca1273
meir485 <meirsimchaleibler@gmail.com>
2020-03-03 21:17:54 +0200

    deploy changed again

---
+++ build/precache-manifest.281707a6048723ccf08e6fc1824e319a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b43f81950786f8d5b38192c716865a",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "903e5517c16427334b4b",
+    "url": "/reactTest/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "2340237f8601f4d649f9",
+    "url": "/reactTest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "903e5517c16427334b4b",
+    "url": "/reactTest/static/js/2.0e5c916b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactTest/static/js/2.0e5c916b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2340237f8601f4d649f9",
+    "url": "/reactTest/static/js/main.a252624a.chunk.js"
+  },
+  {
+    "revision": "d992f2b3065191943454",
+    "url": "/reactTest/static/js/runtime-main.ebffec76.js"
+  }
+]);
