08315036bcf1cfdeec605562fae728c072d4807e
meir485 <meirsimchaleibler@gmail.com>
2020-03-03 20:58:52 +0200

    Updates

---
+++ precache-manifest.842183a3ce6efed61a41619128b21ec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab20747175651e7954f5a0c3301efa6",
+    "url": "/github.com/reactTest.git/index.html"
+  },
+  {
+    "revision": "903e5517c16427334b4b",
+    "url": "/github.com/reactTest.git/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "96ab271033907bad9d4c",
+    "url": "/github.com/reactTest.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "903e5517c16427334b4b",
+    "url": "/github.com/reactTest.git/static/js/2.0e5c916b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/github.com/reactTest.git/static/js/2.0e5c916b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ab271033907bad9d4c",
+    "url": "/github.com/reactTest.git/static/js/main.8ca12e76.chunk.js"
+  },
+  {
+    "revision": "6166331d819abbe94b24",
+    "url": "/github.com/reactTest.git/static/js/runtime-main.4eba7be8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meir485/reactTest.git/precache-manifest.0624fd42ac58504030977eb22a45984d.js"
+  "/github.com/reactTest.git/precache-manifest.842183a3ce6efed61a41619128b21ec8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meir485/reactTest.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/reactTest.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
